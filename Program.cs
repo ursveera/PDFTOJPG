f650e5385abe3fa0a1f5041686f2a10a13e9bed2
Veera9677608806 <Veeramanimeyyappan@gmail.com>
2024-05-09 09:16:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using PDFtoImage;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PDFTOJPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string pdfFilePath = "C:\\Users\\Ursve\\Downloads\\Tamanna Glb\\IT Declaration.pdf"; 
+            byte[] pdfBytes = File.ReadAllBytes(pdfFilePath);
+            string base64String = Convert.ToBase64String(pdfBytes);
+            PDFtoImage.Conversion.SaveJpeg("image.jpg", base64String);
+        }
+    }
+}
